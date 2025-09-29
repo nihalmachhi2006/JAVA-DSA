b7004fa23164ddf1e2b7733b70d9fb5f254a0209
Nihal Machhi <nihalmachhi11@gmail.com>
2025-09-29 19:06:21 +0530

    first commit

---
+++ class_02/main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner SC = new Scanner(System.in);
+        String name = SC.nextLine();
+        System.out.println(name);
+    }
+}
