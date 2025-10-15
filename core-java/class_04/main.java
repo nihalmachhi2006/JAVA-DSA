c32335cd65f3d6cbefac3a20452bd54a919d895b
Nihal Machhi <nihalmachhi11@gmail.com>
2025-10-15 21:14:34 +0530

    Day-3.0

--- core-java/class_04/main.java
+++ core-java/class_04/main.java
@@ -1,22 +1,22 @@
 public class main{
     public static void main(String[] args) {
-        // for(int counter = 1; counter < 11; counter++){
-        //     System.out.println(counter);
-        // }
-        // for(int i=10; i>0; i--){
-        //     System.out.println(i);
-        // }
+        for(int counter = 1; counter < 11; counter++){
+            System.out.println(counter);
+        }
+        for(int i=10; i>0; i--){
+            System.out.println(i);
+        }
 
-        // int i=0;
-        // while(i<11){
-        //     System.out.println(i);
-        //      i = i+1;
-        // }
+        int i=0;
+        while(i<11){
+            System.out.println(i);
+             i = i+1;
+        }
 
-        // do{
-        //     System.out.println(i);
-        //     i=i+1;
-        // }while(i<111);
+        do{
+            System.out.println(i);
+            i=i+1;
+        }while(i<111);
         int sum = 0;
         for(int i = 1; i<=4; i++){
             sum = sum + i;
